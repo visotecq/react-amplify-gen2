571d3a138f14d853500a7e1ba0572f99163939a4
Mudit Shukla <mudit.plausibilitysolutions@gmail.com>
2024-03-15 13:36:12 +0530

    Inital Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import TodoList from "./components/TodoList";
+function App() {
+  return (
+    <>
+      <h1>Hello, Amplify 👋</h1>
+      <TodoList/>
+    </>
+  );
+}
+
+export default withAuthenticator(App);
